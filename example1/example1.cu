b7b3d200a9b4a9870f88dce0b60a473e98818df7
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2020-10-13 10:58:01 +0200

    new commit

--- example1/example1.cu
+++ example1/example1.cu
@@ -27,60 +27,65 @@
 
 int main(int argc, char* argv[])
 {
-  ncclComm_t comms[4];
+  ncclComm_t comms[8];
 
   //managing 4 devices
-  int nDev = 4;
+  int nDev = 8;
 
-  int size = 32*1024*1024;
+  int size = 128*1024*1024;
   //int size = 32*32*32;
   //int size = 8;
 
-  int devs[4] = { 0, 1, 2, 3 };
+  //int devs[4] = { 0, 1, 2, 3 };
+  int devs[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
   //size_t  heapSize = 1024 * 1024 * 1024;
   //cudaDeviceSetLimit(cudaLimitMallocHeapSize, heapSize);
 
 
   //allocating and initializing device buffers
-  int8_t** sendbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
-  int8_t** recvbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
-  int8_t** tempbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
+  //int8_t** sendbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
+  //int8_t** recvbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
+  //int8_t** tempbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
   
-  //float** sendbuff = (float**)malloc(nDev * sizeof(float*));
-  //float** recvbuff = (float**)malloc(nDev * sizeof(float*));
+  float** sendbuff = (float**)malloc(nDev * sizeof(float*));
+  float** recvbuff = (float**)malloc(nDev * sizeof(float*));
+  float** tempbuff = (float**)malloc(nDev * sizeof(float*));
   
   cudaStream_t* s = (cudaStream_t*)malloc(nDev * sizeof(cudaStream_t));
   
-  int8_t* h_sendbuff = (int8_t*)malloc(size * sizeof(int8_t));
-  int8_t* h_recvbuff = (int8_t*)malloc(size * sizeof(int8_t));
+  //int8_t* h_sendbuff = (int8_t*)malloc(size * sizeof(int8_t));
+  //int8_t* h_recvbuff = (int8_t*)malloc(size * sizeof(int8_t));
   
-  //float* h_sendbuff = (float*)malloc(size * sizeof(float));
-  //float* h_recvbuff = (float*)malloc(size * sizeof(float));
+  float* h_sendbuff = (float*)malloc(size * sizeof(float));
+  float* h_recvbuff = (float*)malloc(size * sizeof(float));
   
   for (int i = 0; i < nDev; ++i) {
     CUDACHECK(cudaSetDevice(i));
-    CUDACHECK(cudaMalloc((void**)sendbuff + i, size * sizeof(int8_t)));
-    CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(int8_t)));
-    CUDACHECK(cudaMemset(sendbuff[i], 13, size * sizeof(int8_t)));
-    CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(int8_t)));
+    //CUDACHECK(cudaMalloc((void**)sendbuff + i, size * sizeof(int8_t)));
+    //CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(int8_t)));
+    //CUDACHECK(cudaMemset(sendbuff[i], 13, size * sizeof(int8_t)));
+    //CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(int8_t)));
 
-
-    CUDACHECK(cudaMalloc((void**)tempbuff + i, size * sizeof(int8_t)));
-    CUDACHECK(cudaMemset(tempbuff[i], 0, size * sizeof(int8_t)));
+    //CUDACHECK(cudaMalloc((void**)tempbuff + i, size * sizeof(int8_t)));
+    //CUDACHECK(cudaMemset(tempbuff[i], 0, size * sizeof(int8_t)));
   
   
-    //CUDACHECK(cudaMalloc((void**)sendbuff + i, size * sizeof(float)));
-    //CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(float)));
-    //CUDACHECK(cudaMemset(sendbuff[i], 13, size * sizeof(float)));
-    //CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
+    CUDACHECK(cudaMalloc((void**)sendbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMemset(sendbuff[i], 13, size * sizeof(float)));
+    CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
+
+    CUDACHECK(cudaMalloc((void**)tempbuff + i, size * sizeof(float)));
+    CUDACHECK(cudaMemset(tempbuff[i], 0, size * sizeof(float)));
+
   
   
     CUDACHECK(cudaStreamCreate(s+i));
     CUDACHECK(cudaDeviceSynchronize());
   }
 
-   CUDACHECK(cudaMemcpy(h_sendbuff,sendbuff[0],size * sizeof(int8_t),cudaMemcpyDeviceToHost));
-   //cudaMemcpy(h_sendbuff,sendbuff[0],size * sizeof(float),cudaMemcpyDeviceToHost);
+   //CUDACHECK(cudaMemcpy(h_sendbuff,sendbuff[0],size * sizeof(int8_t),cudaMemcpyDeviceToHost));
+   CUDACHECK(cudaMemcpy(h_sendbuff,sendbuff[0],size * sizeof(float),cudaMemcpyDeviceToHost));
    CUDACHECK(cudaDeviceSynchronize());
    
 
@@ -91,16 +96,17 @@ int main(int argc, char* argv[])
   NCCLCHECK(ncclCommInitAll(comms, nDev, devs));
   NCCLCHECK(ncclGroupStart());
   for (int i = 0; i < nDev; ++i){
-    //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclFloat, ncclSum, comms[i], s[i]));
     //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
-    NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], (void*)tempbuff[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
+    //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclFloat, ncclSum, comms[i], s[i]));
+    //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], (void*)tempbuff[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
+    NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], (void*)tempbuff[i], size, ncclFloat , ncclSum, comms[i], s[i]));
   }
 
   NCCLCHECK(ncclGroupEnd());
 
 
-  CUDACHECK(cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(int8_t),cudaMemcpyDeviceToHost));
-  //cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(float),cudaMemcpyDeviceToHost);
+  //CUDACHECK(cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(int8_t),cudaMemcpyDeviceToHost));
+  CUDACHECK(cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(float),cudaMemcpyDeviceToHost));
   CUDACHECK(cudaDeviceSynchronize());
 
    //for (int i = 0; i< size; ++i) {
@@ -120,14 +126,14 @@ int main(int argc, char* argv[])
    //}
 
    //printf("the first element of the array is: %d \n", h_recvbuff[0]);
-   int count = 0;
-   for(int i=0; i<size; ++i){
-     if(h_recvbuff[i] != 52){
-        count++; 
-        printf("h_recvbuff[%d] = %d \n", i, h_recvbuff[i]);
-     }
-   }
-   printf("count = %d \n", count);
+   //int count = 0;
+   //for(int i=0; i<size; ++i){
+   //  if(h_recvbuff[i] != 52){
+   //     count++; 
+   //     //printf("h_recvbuff[%d] = %d \n", i, h_recvbuff[i]);
+   //  }
+   //}
+   //printf("count = %d \n", count);
 
 
  //synchronizing on CUDA streams to wait for completion of NCCL operation
