321634a7a524d90dc6cb2bbc852ebab0ce56e9fd
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2020-10-14 21:03:49 +0200

    update gitignore

--- example1/example1.cu
+++ example1/example1.cu
@@ -44,7 +44,8 @@ int main(int argc, char* argv[])
   //allocating and initializing device buffers
   int8_t** sendbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
   int8_t** recvbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
-  //int8_t** tempbuff = (int8_t**)malloc(nDev * sizeof(int8_t*));
+  int8_t** tempbuff1 = (int8_t**)malloc(nDev * sizeof(int8_t*));
+  int8_t** tempbuff2 = (int8_t**)malloc(nDev * sizeof(int8_t*));
   
   //float** sendbuff = (float**)malloc(nDev * sizeof(float*));
   //float** recvbuff = (float**)malloc(nDev * sizeof(float*));
@@ -65,8 +66,10 @@ int main(int argc, char* argv[])
     CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(int8_t)));
 
 
-    //CUDACHECK(cudaMalloc((void**)tempbuff + i, size * sizeof(int8_t)));
-    //CUDACHECK(cudaMemset(tempbuff[i], 0, size * sizeof(int8_t)));
+    CUDACHECK(cudaMalloc((void**)tempbuff1 + i, size * sizeof(int8_t)));
+    CUDACHECK(cudaMalloc((void**)tempbuff2 + i, size * sizeof(int8_t)));
+    CUDACHECK(cudaMemset(tempbuff1[i], 0, size * sizeof(int8_t)));
+    CUDACHECK(cudaMemset(tempbuff2[i], 0, size * sizeof(int8_t)));
   
   
     //CUDACHECK(cudaMalloc((void**)sendbuff + i, size * sizeof(float)));
@@ -90,13 +93,13 @@ int main(int argc, char* argv[])
   NCCLCHECK(ncclGroupStart());
   for (int i = 0; i < nDev; ++i){
     //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclFloat, ncclSum, comms[i], s[i]));
-    NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
-    //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], (void*)tempbuff[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
+    //NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
+    NCCLCHECK(ncclAllReduce((const void*)sendbuff[i], (void*)recvbuff[i], (void*)tempbuff1[i], (void*)tempbuff2[i], size, ncclInt8 , ncclSum, comms[i], s[i]));
   }
 
   NCCLCHECK(ncclGroupEnd());
 
-  //CUDACHECK(cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(int8_t),cudaMemcpyDeviceToHost));
+  CUDACHECK(cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(int8_t),cudaMemcpyDeviceToHost));
   //cudaMemcpy(h_recvbuff,recvbuff[0],size * sizeof(float),cudaMemcpyDeviceToHost);
   //CUDACHECK(cudaDeviceSynchronize());
 
