c7cba2c2a901aa9deab6e629e780d83d5b94e9fd
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2021-01-08 11:29:56 +0100

    next commit

--- example1/example1.cu
+++ example1/example1.cu
@@ -30,22 +30,23 @@
 
 int main(int argc, char* argv[])
 {
+  //ncclComm_t comms[2];
   ncclComm_t comms[4];
 
   //managing 4 devices
   //int nDev = 1;
-  int nDev = 2;
-  //int nDev = 4;
+  //int nDev = 2;
+  int nDev = 4;
 
-  //int size = 256*1024*1024;
+  //int size = 16*1024*1024;
   //int size = 128*32*32;
   //int size = 8192;
   int size = 8;
 
 
   //int devs[1] = { 0 };
-  int devs[2] = { 0, 1 };
-  //int devs[4] = { 0, 1, 2, 3 };
+  //int devs[2] = { 0, 1 };
+  int devs[4] = { 0, 1, 2, 3 };
   //size_t  heapSize = 1024 * 1024 * 1024;
   //cudaDeviceSetLimit(cudaLimitMallocHeapSize, heapSize);
 
@@ -82,26 +83,26 @@ int main(int argc, char* argv[])
     CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(float)));
     //CUDACHECK(cudaMalloc((void**)recvbuff + i, size * sizeof(int8_t)));
 
-    float fill_value1 = 0.014;
-    float fill_value2 = 0.011;
-    float fill_value3 = 0.012;
-    float fill_value4 = 0.013;
-    float fill_value5 = 0.015;
-    float fill_value6 = 0.016;
-    float fill_value7 = 0.020;
-    float fill_value8 = 0.017;
-    thrust::device_ptr<float> dev_ptr1(sendbuff[i]);
-    thrust::fill(dev_ptr1, dev_ptr1 + 1, fill_value1);
-    thrust::fill(dev_ptr1 + 1, dev_ptr1 + 2, fill_value2);
-    thrust::fill(dev_ptr1 + 2, dev_ptr1 + 3, fill_value3);
-    thrust::fill(dev_ptr1 + 3, dev_ptr1 + 4, fill_value4);
-    thrust::fill(dev_ptr1 + 4, dev_ptr1 + 5, fill_value5);
-    thrust::fill(dev_ptr1 + 5, dev_ptr1 + 6, fill_value6);
-    thrust::fill(dev_ptr1 + 6, dev_ptr1 + 7, fill_value7);
-    thrust::fill(dev_ptr1 + 7, dev_ptr1 + 8, fill_value8);
-
+    //float fill_value1 = 0.014;
+    //float fill_value2 = 0.011;
+    //float fill_value3 = 0.012;
+    //float fill_value4 = 0.013;
+    //float fill_value5 = 0.015;
+    //float fill_value6 = 0.016;
+    //float fill_value7 = 0.020;
+    //float fill_value8 = 0.017;
     //thrust::device_ptr<float> dev_ptr1(sendbuff[i]);
-    //thrust::fill(dev_ptr1, dev_ptr1 + size, 2.4);
+    //thrust::fill(dev_ptr1, dev_ptr1 + 1, fill_value1);
+    //thrust::fill(dev_ptr1 + 1, dev_ptr1 + 2, fill_value2);
+    //thrust::fill(dev_ptr1 + 2, dev_ptr1 + 3, fill_value3);
+    //thrust::fill(dev_ptr1 + 3, dev_ptr1 + 4, fill_value4);
+    //thrust::fill(dev_ptr1 + 4, dev_ptr1 + 5, fill_value5);
+    //thrust::fill(dev_ptr1 + 5, dev_ptr1 + 6, fill_value6);
+    //thrust::fill(dev_ptr1 + 6, dev_ptr1 + 7, fill_value7);
+    //thrust::fill(dev_ptr1 + 7, dev_ptr1 + 8, fill_value8);
+
+    thrust::device_ptr<float> dev_ptr1(sendbuff[i]);
+    thrust::fill(dev_ptr1, dev_ptr1 + size, 2.4);
 
     thrust::device_ptr<float> dev_ptr2(recvbuff[i]);
     thrust::fill(dev_ptr2, dev_ptr2 + size, 0.0);
@@ -141,22 +142,27 @@ int main(int argc, char* argv[])
   CUDACHECK(cudaMemcpy(h_recvbuff, recvbuff[0], size * sizeof(float),cudaMemcpyDeviceToHost));
   CUDACHECK(cudaDeviceSynchronize());
 
-  //for (int i = 0; i< size; ++i) {
-  //  printf("%f\n",h_sendbuff[i]);
-  //}
+  for (int i = 0; i< size; ++i) {
+    printf("%f\n",h_sendbuff[i]);
+  }
  
-  //for (int i = 0; i< size; ++i) {
-  //  printf("%f\n",h_recvbuff[i]);
-  //}
+  for (int i = 0; i< size; ++i) {
+    printf("%f\n",h_recvbuff[i]);
+  }
 
-  //int count = 0;
-  //for(int i=0; i<size; ++i){
-  //  if(abs(h_recvbuff[i] - 9.6) > 0.0001f){
-  //     count++; 
-  //     //printf("h_recvbuff[%d] = %f \n", i, h_recvbuff[i]);
-  //  }
-  //}
-  //printf("count = %d \n", count);
+  //bool is_first_index = true;
+  int count = 0;
+  for(int i=0; i<size; ++i) {
+    if(abs(h_recvbuff[i] - 9.6) > 0.0001f){
+       count++;
+       //printf("h_recvbuff[%d] = %f \n", i, h_recvbuff[i]);
+       //if (is_first_index) {
+       //  printf("first_index = %d\n", i);
+       //  is_first_index = false;
+       //}
+    }
+  }
+  printf("count = %d \n", count);
 
  //synchronizing on CUDA streams to wait for completion of NCCL operation
   for (int i = 0; i < nDev; ++i) {
